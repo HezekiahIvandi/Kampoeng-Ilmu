cc8266b483f4d98839d4d05f9caa222adadcc689
HezekiahIvandi <hezekiahivandi@gmail.com>
2024-04-03 15:30:20 +0700

    Menambahkan page Daftar

---
+++ public/js/daftar.js
@@ -0,0 +1,23 @@
+const hidePassword = document.getElementById("hide-password");
+const passwordInput = document.getElementById("password");
+const passwordStrengthMessage = document.getElementById(
+  "password-strength-message"
+);
+hidePassword.addEventListener("click", function () {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+});
+
+passwordInput.addEventListener("focus", function () {
+  // Show the password strength message when input gets focus
+  passwordStrengthMessage.style.display = "block";
+});
+
+// Add blur event listener to password input
+passwordInput.addEventListener("blur", function () {
+  // Hide the password strength message when input loses focus
+  passwordStrengthMessage.style.display = "none";
+});
