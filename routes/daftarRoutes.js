edad3903aa49d69da41e1140e367eb032841800c
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2024-04-04 21:29:01 +0700

    menambah autentikasi daftar dan login

--- routes/daftarRoutes.js
+++ routes/daftarRoutes.js
@@ -1,5 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const User = require("../models/user");
+
+router.get("/user/dashboard", (req, res) => {
+  res.send("login succesfull!");
+});
+
+router.get("/login", (req, res) => {
+  res.send("Silahkan Login");
+});
+
+router.post(
+  "/login/submit",
+  passport.authenticate("local", {
+    successRedirect: "/user/dashboard",
+    failureRedirect: "/login",
+  })
+);
+
+router.post("/daftar/submit", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if the username is already taken
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).send("Email already exists");
+    }
+
+    // Hash the password before saving it
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new admin user
+    const newUser = new User({
+      email,
+      password: hashedPassword, // Store the hashed password in the database
+    });
+
+    // Save the new user to the database
+    await newUser.save();
+    res.status(201).send("Admin account created successfully");
+  } catch (error) {
+    console.error("Error creating admin account:", error);
+    res.status(500).send("Error creating admin account");
+  }
+});
 
 router.get("/daftar", (req, res) => {
   res.render("daftar.ejs", {
@@ -7,5 +54,4 @@ router.get("/daftar", (req, res) => {
     layout: "mainLayout.ejs",
   });
 });
-
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
+const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
 const indexRoutes = require("./routes/IndexRoutes");
 const daftarRoutes = require("./routes/daftarRoutes");
@@ -7,6 +9,20 @@ const kelasRoutes = require("./routes/kelasRoutes");
 const alurRoutes = require("./routes/alurRoutes");
 const tentangRoutes = require("./routes/tentangRoutes");
 const port = 3000;
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(express.json());
+mongoose.connect("mongodb://localhost:27017/KampoengIlmu", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+require("./passport-config");
 app.set("view engine", "ejs");
 
 //static
