6784446b62ff0c4ac09a541a11402968cc689685
HezekiahIvandi <hezekiahivandi@gmail.com>
2024-04-05 20:57:11 +0700

    cards info

--- routes/IndexRoutes.js
+++ routes/IndexRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
+const { berandaCardsInfo } = require("../views/layouts/cards-info");
 router.get("/", (req, res) => {
   res.render("index.ejs", {
     title: "Home",
     layout: "mainLayout.ejs",
+    cardsInfo: berandaCardsInfo,
   });
 });
 
--- routes/kelasRoutes.js
+++ routes/kelasRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { ruangKelasCardsInfo } = require("../views/layouts/cards-info");
 
 router.get("/kelas-belajar", (req, res) => {
   res.render("kelas", {
     title: "Kelas Belajar",
     layout: "mainLayout.ejs",
+    cardsInfo: ruangKelasCardsInfo,
   });
 });
 
